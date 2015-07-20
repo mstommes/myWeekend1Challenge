ac34705a5110e5543762e376eba2622f6fcddb68
[MStommes] <mstommes@gmail.com>
2015-07-20 12:57:57 -0500

    second commit

--- app.js
+++ app.js
@@ -9,18 +9,15 @@ $(document).ready(function(){
 			values[this.name] = $(this).val();
 		});
 
-		console.log($inputs);
-		console.log(values);
-		console.log(values.firstName);
-		console.log(values.lastName);
-
 		insertEmployeeCard(values);
 
-	
+	});
+	$("[name='deleteButton']").on('click', function(event){
+		event.preventDefault();
+
+		$("#employeeInput").children(".employee").last().remove();              
 
 	});
-	
-	
 });
 	
 function insertEmployeeCard(values){
@@ -28,19 +25,33 @@ function insertEmployeeCard(values){
 	var $myDiv = $("<div/>",  {
 		class:"employee"
 	});
+
+	//this code adds each object to the div with a class of employee
 	$myDiv.append("<p> First Name is: " + values.firstName + "</p>");
 	$myDiv.append("<p> Last Name is: " + values.lastName + "</p>");
 	$myDiv.append("<p> Title is: " + values.title + "</p>");
 	$myDiv.append("<p> Employee ID Number is: " + values.employeeNumber+ "</p>");
 	$myDiv.append("<p> Last Review Score was: <span style = 'color:" + reviewScoreColorMaker(values.lastReviewScore) +"'>" + values.lastReviewScore + "</span></p>");
 	$myDiv.append("<p> Annual Salary is: " + values.salary + "</p>");
-	$el.append($myDiv);
-	$("[name='deleteButton']").on('click', function(event){
-	$el.remove("#employeeInput");              
+	
+	var $button = $("<input type=\"button\" value=\"Delete This Item\" class=\"deleteThisButton\">");
+
+	$button.on('click', function(event){
+		event.preventDefault();
+
+		$(this).parent().remove();              
+
 	});
 
-	
-	
+	$myDiv.append($button);
+
+
+	//this is the code that deletes the appended elements from the previous class
+	// $("someForm").append($("[name='deleteButton']").on('click', function(event){
+	// 	$("#employeeInput").children(".employee").last().remove();              
+	// 	}));	
+
+	$el.append($myDiv);
 }
 
 
@@ -74,7 +85,7 @@ function reviewScoreColorMaker(rating) {
 }	
 
 /*
-function randomEmployeeGenerator {
+function randomEmployeeGenerator(firstName) {
 	function randomNumber(min, max) {
 	return Math.floor(Math.random() * (1 + max - min) + min);}
 
